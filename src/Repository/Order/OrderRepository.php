9a24cce69d32929487123ba5492252acaf64feb9
Tomwebdesign <thomasmartinez260@gmail.com>
2024-06-06 16:54:34 +0200

    feat(order): add Factory & Manager & SessionStorage for order

--- src/Repository/Order/OrderRepository.php
+++ src/Repository/Order/OrderRepository.php
@@ -3,7 +3,9 @@
 namespace App\Repository\Order;
 
 use App\Entity\Order\Order;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Mapping\OrderBy;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,6 +16,19 @@ class OrderRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Order::class);
+    }
+
+    public function findLastCartByUser(User $user): ?Order
+    {
+        return $this->createQueryBuilder('o')
+            ->andWhere('o.status = :status')
+            ->setParameter('status', Order::STATUS_CART)
+            ->join('o.user', 'u', 'WITH', 'u = :user')
+            ->setParameter('user', $user)
+            ->OrderBy('o.createdAt', 'DESC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
     //    /**
